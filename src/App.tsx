c5fd1a7c5460ea17797457d1ef4b8d408426429b
Bojan Blazevski <blazevski.bojan1@gmail.com>
2022-09-17 15:41:07 +0200

    Add main project src files

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import { NavigationBar } from './components/navigation/NavigationBar';
+
+function App() {
+  return (
+    <div className="App">
+      <NavigationBar />
+    </div>
+  );
+}
+
+export default App;
