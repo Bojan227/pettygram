00be0ca8cf46897b3434ceba454bcfab4d8d3296
Bojan Blazevski <blazevski.bojan1@gmail.com>
2022-10-22 12:05:48 +0200

    Add logout functionality

--- src/components/navigation/FooterMenu.tsx
+++ src/components/navigation/FooterMenu.tsx
@@ -1,14 +1,17 @@
 import { Link } from 'react-router-dom';
+import useLogout from '../../hooks/useLogout';
 
 type FooterMenuProps = { toggleMore?: boolean };
 
 export function FooterMenu({ toggleMore }: FooterMenuProps): JSX.Element {
+  const { logout } = useLogout();
+
   return (
     <div
       className="footer-menu-section"
       style={{ display: `${toggleMore ? 'flex' : ''}` }}
     >
-      <Link to="/">
+      <Link to="/" onClick={logout}>
         <h4>Logout</h4>
       </Link>
       <Link to="profile">
