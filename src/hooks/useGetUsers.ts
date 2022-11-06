658f98f1161d7950cdfb18e890e946034c7ae33b
Bojan Blazevski <blazevski.bojan1@gmail.com>
2022-11-06 19:55:01 +0100

    Import UserType for the users state

--- src/hooks/useGetUsers.ts
+++ src/hooks/useGetUsers.ts
@@ -1,31 +1,16 @@
 import { useState } from 'react';
-import useUserContenxt from '../hooks/useUserContext';
-
-export interface User {
-  _id: string;
-  username: string;
-  firstName: string;
-  lastName: string;
-  imageId: string;
-  imageUrl: string;
-  followers: [string];
-  following: [string];
-}
+import { UserType } from '../context/userContext';
 
 export const useGetUsers = () => {
-  const [users, setUsers] = useState<User[]>();
+  const [users, setUsers] = useState<UserType[]>();
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState('');
 
-  const userContext = useUserContenxt();
-
   const getUsers = async () => {
     setIsLoading(true);
-    console.log(userContext?.user._id);
 
     try {
       const res = await fetch('http://localhost:4000/user/');
-
       const json = await res.json();
 
       setUsers([...json]);
