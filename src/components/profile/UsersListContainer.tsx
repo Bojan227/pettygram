49a3321277b15fb848ff4f909c8461629e4550f9
Bojan Blazevski <blazevski.bojan1@gmail.com>
2022-11-13 13:24:13 +0100

    Add Users list container component

---
+++ src/components/profile/UsersListContainer.tsx
@@ -0,0 +1,28 @@
+import { UserCard } from '../UserCard';
+import { UserType } from '../../context/userContext';
+
+export const UsersListContainer = ({
+  users,
+  closeModal,
+  title,
+}: {
+  users: (UserType | undefined)[] | undefined;
+  closeModal: React.Dispatch<React.SetStateAction<boolean>>;
+  title: string;
+}) => {
+  return (
+    <div onClick={() => closeModal(false)} className="users-container-list">
+      <div className="users-list-content">
+        <header>
+          <h1>{title}</h1>
+          <h1>X</h1>
+        </header>
+        <div>
+          {users?.map((user) => (
+            <UserCard key={user?._id} {...user} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
