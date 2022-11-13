803604a94fa37ff8b2b94d61894e13a1e8cd912c
Bojan Blazevski <blazevski.bojan1@gmail.com>
2022-11-13 13:35:23 +0100

    Set new state for toggling UsersListContainer component and update useEffect dependency array

--- src/components/profile/ProfileCard.tsx
+++ src/components/profile/ProfileCard.tsx
@@ -5,10 +5,14 @@ import { Post } from '../../hooks/useGetPosts';
 import { useFollow } from '../../hooks/useFollow';
 import useUserContext from '../../hooks/useUserContext';
 import { useGetData } from '../../hooks/useGetData';
+import { UsersListContainer } from './UsersListContainer';
 
 export const ProfileCard = () => {
   const [userInfo, setUserInfo] = useState<UserType>();
   const [userPosts, setUserPosts] = useState<Post[] | null>(null);
+  const [showFollowers, setShowFollowers] = useState<boolean>(false);
+  const [showFollowing, setShowFollowing] = useState<boolean>(false);
+
   const { userId } = useParams();
   const { changeFollowStatus } = useFollow();
   const { getData } = useGetData();
@@ -23,21 +27,38 @@ export const ProfileCard = () => {
       url: `http://localhost:4000/posts/${userId}`,
       setState: setUserPosts,
     });
-  }, [userId]);
+  }, [userId, userContext?.user.following]);
 
   return (
     <div className="profile-card">
+      {showFollowers && (
+        <UsersListContainer
+          title={'Followers'}
+          users={userInfo?.followers}
+          closeModal={setShowFollowers}
+        />
+      )}
+      {showFollowing && (
+        <UsersListContainer
+          title={'Following'}
+          users={userInfo?.following}
+          closeModal={setShowFollowing}
+        />
+      )}
+
       <img src={userInfo?.imageUrl} alt="img" />
       <div className="info-profile">
         <div className="info-followers">
           <h4>
             {userPosts?.length} {userPosts?.length === 1 ? 'post' : 'posts'}
           </h4>
-          <h4>
+          <h4 onClick={() => setShowFollowers(true)}>
             {userInfo?.followers?.length}{' '}
             {userInfo?.followers?.length === 1 ? 'follower' : 'followers'}
           </h4>
-          <h4>{userInfo?.following?.length} following</h4>
+          <h4 onClick={() => setShowFollowing(true)}>
+            {userInfo?.following?.length} following
+          </h4>
         </div>
         <h2>{userInfo?.firstName + ' ' + userInfo?.lastName}</h2>
         {userContext?.user._id === userId ? (
