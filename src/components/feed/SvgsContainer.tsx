98a9fbd74d709c641b2767f69d21ee5c86505728
Bojan Blazevski <blazevski.bojan1@gmail.com>
2022-11-20 17:35:37 +0100

    Add edit svg

--- src/components/feed/SvgsContainer.tsx
+++ src/components/feed/SvgsContainer.tsx
@@ -10,7 +10,6 @@ type LikeButtonProps = {
 export const LikeButton = ({ updateLike, likes }: LikeButtonProps) => {
   const userContext = useUserContenxt();
 
-  console.log(likes);
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -57,7 +56,6 @@ export const Bookmark = ({ postId }: { postId: string }) => {
   const userContext = useUserContenxt();
   const { updateSaved } = useUpdateSaved();
 
-  console.log(userContext?.user.saved);
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -76,6 +74,25 @@ export const Bookmark = ({ postId }: { postId: string }) => {
         strokeLinecap="round"
         strokeLinejoin="round"
         d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z"
+      />
+    </svg>
+  );
+};
+export const Edit = ({ onClick }: { onClick: () => void }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth="1.5"
+      stroke="currentColor"
+      className="w-6 h-6 cursor-pointer"
+      onClick={onClick}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
       />
     </svg>
   );
