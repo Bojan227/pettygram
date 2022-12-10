ceed036cb9de80304b2d4d078fc5294e74fe094b
Bojan Blazevski <blazevski.bojan1@gmail.com>
2022-12-10 13:43:42 +0100

    Add custom hook for editing user information

---
+++ src/hooks/useEditInfo.ts
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+import useUserContenxt from './useUserContext';
+
+export default function useEditInfo() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState('');
+  const userContext = useUserContenxt();
+
+  const editInfo = async (
+    username: string,
+    firstName: string,
+    lastName: string
+  ) => {
+    setIsLoading(true);
+    try {
+      const user = await fetch('http://localhost:4000/user/edit', {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${document.cookie.split('=')[1]}`,
+        },
+        body: JSON.stringify({
+          username,
+          firstName,
+          lastName,
+        }),
+      });
+
+      const updatedUser = await user.json();
+
+      userContext?.dispatch({ type: 'LOGIN', payload: updatedUser });
+      localStorage.setItem('user', JSON.stringify(updatedUser));
+    } catch (error) {
+      if (error instanceof Error) {
+        setError(error.message);
+      }
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return { editInfo, isLoading, error };
+}
