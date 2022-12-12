588369623c29b6577016838d7e81dca17a49b8e2
Bojan Blazevski <blazevski.bojan1@gmail.com>
2022-12-12 16:20:28 +0100

    Add: Chat header for current selected user

---
+++ src/components/chat/ChatHeader.tsx
@@ -0,0 +1,17 @@
+import { UserType } from '../../context/userContext';
+
+export default function ChatHeader({
+  selectedUser,
+}: {
+  selectedUser: UserType | undefined;
+}) {
+  return (
+    <>
+      <div className="private-chat-header">
+        <img src={selectedUser?.imageUrl} />
+        <h1>{selectedUser?.firstName}</h1>
+        <h1>{selectedUser?.lastName}</h1>
+      </div>
+    </>
+  );
+}
