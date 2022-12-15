e812a4ab6afee128a30498d72728cdbdec3f24c8
Bojan Blazevski <blazevski.bojan1@gmail.com>
2022-12-15 15:22:18 +0100

    Check if selectedUser is true before rendering the content

--- src/components/chat/ChatHeader.tsx
+++ src/components/chat/ChatHeader.tsx
@@ -6,12 +6,14 @@ export default function ChatHeader({
   selectedUser: UserType | undefined;
 }) {
   return (
-    <>
-      <div className="private-chat-header">
-        <img src={selectedUser?.imageUrl} />
-        <h1>{selectedUser?.firstName}</h1>
-        <h1>{selectedUser?.lastName}</h1>
-      </div>
-    </>
+    <div className="private-chat-header">
+      {selectedUser && (
+        <>
+          <img src={selectedUser?.imageUrl} />
+          <h1>{selectedUser?.firstName}</h1>
+          <h1>{selectedUser?.lastName}</h1>
+        </>
+      )}
+    </div>
   );
 }
