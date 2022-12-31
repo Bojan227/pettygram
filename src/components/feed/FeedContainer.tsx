7bad4aca1d2b1b62c46a0587d8d8675770d0ef8f
Bojan Blazevski <blazevski.bojan1@gmail.com>
2022-12-31 17:44:03 +0100

    Add useEffect for listening new messages

--- src/components/feed/FeedContainer.tsx
+++ src/components/feed/FeedContainer.tsx
@@ -5,6 +5,10 @@ import useUserContext from '../../hooks/useUserContext';
 import './feed.css';
 import { FeedContainerProps } from './types/feedTypes';
 import { UserCard } from '../UserCard';
+import { socket } from '../../constants/socket';
+import { ChatType } from '../../context/chatDataContext';
+import { useAddNotificationMessage } from '../../context/notificationsMessagesContext';
+import { useNewMessages } from '../../context/notificationsMessagesContext';
 
 export const FeedContainer = ({
   posts,
@@ -13,9 +17,26 @@ export const FeedContainer = ({
 }: FeedContainerProps) => {
   const { getUsers, isLoading, users } = useGetUsers();
   const userContext = useUserContext();
+  const addNotificationMessage = useAddNotificationMessage();
 
   useEffect(() => {
     getUsers();
+  }, []);
+
+  useEffect(() => {
+    if (userContext?.user._id) {
+      socket.emit('add_user', { userId: userContext?.user?._id });
+    }
+  }, []);
+
+  useEffect(() => {
+    socket.on('notification_message', (data: ChatType) => {
+      console.log(data);
+      addNotificationMessage(data);
+    });
+    return () => {
+      socket.off('notification_message');
+    };
   }, []);
 
   const filteredUsers = users?.filter(
@@ -28,18 +49,19 @@ export const FeedContainer = ({
 
   return (
     <main className="feed-container">
-      {error && <h1>{error}</h1>}
       {posts?.map((post, i) => (
         <PostCard key={i} {...post} setPosts={setPosts} />
       ))}
-      <div className="suggested-users">
-        <div className="suggested-header">
-          <h1>Discover People</h1>
+      {filteredUsers?.length === 0 ? null : (
+        <div className="suggested-users">
+          <div className="suggested-header">
+            <h1>Discover People</h1>
+          </div>
+          {filteredUsers?.map(({ imageUrl, firstName, lastName, _id }) => (
+            <UserCard key={_id} {...{ imageUrl, firstName, lastName, _id }} />
+          ))}
         </div>
-        {filteredUsers?.map(({ imageUrl, firstName, lastName, _id }) => (
-          <UserCard key={_id} {...{ imageUrl, firstName, lastName, _id }} />
-        ))}
-      </div>
+      )}
     </main>
   );
 };
