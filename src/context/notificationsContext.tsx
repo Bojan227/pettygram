5b92ee00134ede089f42b079c287765cf4f2e30c
Bojan Blazevski <blazevski.bojan1@gmail.com>
2023-01-02 16:06:00 +0100

    Add additional action type method

--- src/context/notificationsContext.tsx
+++ src/context/notificationsContext.tsx
@@ -2,6 +2,7 @@ import { createContext, useCallback, useReducer, useContext } from 'react';
 import { UserType } from './userContext';
 
 type ActionType =
+  | { type: 'ADD_NOTIFICATIONS'; payload: Notification[] }
   | { type: 'ADD_NOTIFICATION'; payload: Notification }
   | { type: 'FILTER_NOTIFICATIONS' };
 
@@ -17,18 +18,22 @@ type NotificationsManagerResult = ReturnType<typeof notificationsManager>;
 
 const NotificationsContext = createContext<NotificationsManagerResult>({
   notifications: [],
+  addNotifications: () => {},
   addNotification: () => {},
   filterNotifications: () => {},
 });
 
 function notificationsManager(initialData: Notification[]): {
   notifications: Notification[];
+  addNotifications: (notifications: Notification[]) => void;
   addNotification: (notification: Notification) => void;
   filterNotifications: () => void;
 } {
   const [notifications, dispatch] = useReducer(
     (state: Notification[], action: ActionType) => {
       switch (action.type) {
+        case 'ADD_NOTIFICATIONS':
+          return action.payload;
         case 'ADD_NOTIFICATION':
           return [...state, action.payload];
         case 'FILTER_NOTIFICATIONS':
@@ -39,6 +44,9 @@ function notificationsManager(initialData: Notification[]): {
     },
     initialData
   );
+  const addNotifications = useCallback((notifications: Notification[]) => {
+    dispatch({ type: 'ADD_NOTIFICATIONS', payload: notifications });
+  }, []);
 
   const addNotification = useCallback((notification: Notification) => {
     dispatch({ type: 'ADD_NOTIFICATION', payload: notification });
@@ -48,7 +56,12 @@ function notificationsManager(initialData: Notification[]): {
     dispatch({ type: 'FILTER_NOTIFICATIONS' });
   }, []);
 
-  return { notifications, addNotification, filterNotifications };
+  return {
+    notifications,
+    addNotification,
+    filterNotifications,
+    addNotifications,
+  };
 }
 
 export const NotificationsProvider: React.FunctionComponent<{
@@ -67,6 +80,12 @@ export const useNotifications = (): Notification[] => {
   const { notifications } = useContext(NotificationsContext);
   return notifications;
 };
+
+export const useAddNotifications =
+  (): NotificationsManagerResult['addNotifications'] => {
+    const { addNotifications } = useContext(NotificationsContext);
+    return addNotifications;
+  };
 
 export const useAddNotification =
   (): NotificationsManagerResult['addNotification'] => {
