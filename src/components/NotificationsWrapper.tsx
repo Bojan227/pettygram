7b9967450cecf8560d748fe1bbb54a414389ece6
Bojan Blazevski <blazevski.bojan1@gmail.com>
2023-01-03 17:06:51 +0100

    Fetch notifications data on component mount

--- src/components/NotificationsWrapper.tsx
+++ src/components/NotificationsWrapper.tsx
@@ -2,6 +2,9 @@ import { ReactNode, useEffect } from 'react';
 import { useAddNotification } from '../context/notificationsContext';
 import { socket } from '../constants/socket';
 import { Notification } from '../context/notificationsContext';
+import useGetNotificationsData from '../hooks/useGetNotificationsData';
+import useUserContext from '../hooks/useUserContext';
+import { useNotifications } from '../context/notificationsContext';
 
 export default function NotificationsWrapper({
   children,
@@ -9,6 +12,13 @@ export default function NotificationsWrapper({
   children: ReactNode;
 }) {
   const addNotification = useAddNotification();
+  const { getNotificationsByReceiverId, error } = useGetNotificationsData();
+  const userContext = useUserContext();
+
+  useEffect(() => {
+    getNotificationsByReceiverId(userContext?.user._id!);
+  }, [userContext?.user._id]);
+
   useEffect(() => {
     socket.on('receive_notification', (data: Notification) => {
       console.log(data);
@@ -22,6 +32,7 @@ export default function NotificationsWrapper({
 
   return (
     <main className="flex bg-stone-50 justify-center min-h-screen">
+      {error && error}
       {children}
     </main>
   );
