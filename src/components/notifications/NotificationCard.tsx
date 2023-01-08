ad71ecbda7fec4b64791321bcde30dc3763e69b7
Bojan Blazevski <blazevski.bojan1@gmail.com>
2023-01-08 14:37:34 +0100

    Add link to specific profile on notificiations card

--- src/components/notifications/NotificationCard.tsx
+++ src/components/notifications/NotificationCard.tsx
@@ -1,10 +1,12 @@
 import { formatDistanceToNow } from 'date-fns';
+import { Link } from 'react-router-dom';
 
 type NotificatonCardProps = {
   imageUrl?: string | undefined;
   username?: string;
   message: string;
   createdAt: string;
+  _id?: string | undefined;
 };
 import default_insta from '../../assets/default_insta.jpg';
 
@@ -13,16 +15,22 @@ export default function NotificationCard({
   username,
   message,
   createdAt,
+  _id,
 }: NotificatonCardProps) {
   return (
     <div onClick={(e) => e.stopPropagation()} className="notification-card">
       <img src={imageUrl || default_insta} />
+
       <div className="notification-content">
-        <p>
-          {`${username} ${message} ${
-            createdAt ? formatDistanceToNow(new Date(createdAt).getTime()) : ''
-          }`}
-        </p>
+        <Link to={`/profile/${_id}`}>
+          <p>
+            {`${username} ${message} ${
+              createdAt
+                ? formatDistanceToNow(new Date(createdAt).getTime())
+                : ''
+            }`}
+          </p>
+        </Link>
       </div>
     </div>
   );
