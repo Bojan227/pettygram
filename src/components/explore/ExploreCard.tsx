105d61d934bc37c246b037e9e7d7751d2e482c89
Bojan Blazevski <blazevski.bojan1@gmail.com>
2023-01-10 19:55:13 +0100

    Add Explore Card component and Link to the specific post

---
+++ src/components/explore/ExploreCard.tsx
@@ -0,0 +1,22 @@
+const cardSize = ['card_small', 'card_medium', 'card_large'];
+import { Link } from 'react-router-dom';
+
+export default function ExploreCard({
+  imageUrl,
+  _id,
+}: {
+  imageUrl: string;
+  _id: string;
+}) {
+  return (
+    <div
+      className={`card-explore ${
+        cardSize[Math.floor(Math.random() * (2 - 0 + 1) + 0)]
+      }`}
+    >
+      <Link to={`/p/${_id}`}>
+        <img src={imageUrl} />
+      </Link>
+    </div>
+  );
+}
--- src/components/explore/ExploreContainer.tsx
+++ src/components/explore/ExploreContainer.tsx
@@ -1,26 +1,15 @@
 import { Post } from '../feed/types/feedTypes';
-import { v4 as uuidv4 } from 'uuid';
 import './explore.css';
-import { postsData } from './postsData';
-
-const cardSize = ['card_small', 'card_medium', 'card_large'];
+import { v4 as uuidv4 } from 'uuid';
+import { Link } from 'react-router-dom';
+import ExploreCard from './ExploreCard';
 
 export default function ExploreContainer({ posts }: { posts: Post[] }) {
-  console.log(posts);
   return (
     <div className="explore-container">
-      {posts.map((post, i) => {
-        return (
-          <div
-            key={uuidv4()}
-            className={`card-explore ${
-              cardSize[Math.floor(Math.random() * (2 - 0 + 1) + 0)]
-            }`}
-          >
-            <img src={post.imageUrl} />
-          </div>
-        );
-      })}
+      {posts.map((post) => (
+        <ExploreCard key={uuidv4()} {...{ ...post }} />
+      ))}
     </div>
   );
 }
