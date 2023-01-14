94b12483a91503454f4445eaa317919376573590
Bojan Blazevski <blazevski.bojan1@gmail.com>
2023-01-14 16:47:47 +0100

    Add DropFileContainer for uploading new images to the users new posts

---
+++ src/components/navigation/DropFileContainer.tsx
@@ -0,0 +1,46 @@
+import { DragEvent } from 'react';
+import { Dispatch, SetStateAction } from 'react';
+
+export default function DropFileContainer({
+  handleImageUpload,
+  setFiles,
+}: {
+  handleImageUpload: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  setFiles: Dispatch<SetStateAction<File[]>>;
+}) {
+  const onDragOver = (e: DragEvent<HTMLDivElement>) => {
+    e.stopPropagation();
+    e.preventDefault();
+  };
+
+  const onDragEnter = (e: DragEvent<HTMLDivElement>) => {
+    e.stopPropagation();
+  };
+
+  const handleDrop = (e: DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    setFiles([...e.dataTransfer.files]);
+  };
+
+  return (
+    <div
+      className="drop-div"
+      onDragOver={(e) => onDragOver(e)}
+      onDragEnter={(e) => onDragEnter(e)}
+      onDrop={(e) => handleDrop(e)}
+      onClick={(e) => e.stopPropagation()}
+    >
+      <h1 className="p-4">Create new post</h1>
+      <h1>Drag Photos and videos here</h1>
+      <input
+        onChange={(e) => handleImageUpload(e)}
+        type="file"
+        id="actual-btn"
+      />
+
+      <label htmlFor="actual-btn">Choose File</label>
+    </div>
+  );
+}
