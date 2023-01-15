23474a34129365eb79db47c0bf2a62c427868a24
Bojan Blazevski <blazevski.bojan1@gmail.com>
2023-01-15 12:42:03 +0100

    Send an array of base64 strings to the backend

--- src/hooks/useCreatePost.ts
+++ src/hooks/useCreatePost.ts
@@ -1,55 +1,48 @@
 import { useState, Dispatch, SetStateAction } from 'react';
 import { Post } from '../components/feed/types/feedTypes';
 import fetcher from '../api/fetcher';
+import { fileReader } from '../utils/fileReader';
 
 export default function useCreatePost() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState('');
   const [message, setMessage] = useState('');
 
-  const createpost = async (
+  const createPost = async (
     text: string,
-    image: File,
+    files: File[],
     setPosts: Dispatch<SetStateAction<Post[] | undefined>>
   ) => {
     setIsLoading(true);
+    try {
+      const images = await fileReader(files);
+      const post = await fetcher('http://localhost:4000/posts/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${
+            document.cookie
+              ?.split('; ')
+              ?.find((value) => value?.includes('token'))
+              ?.split('=')[1]
+          }`,
+        },
+        body: JSON.stringify({
+          text,
+          images,
+        }),
+      });
 
-    const reader: any = new FileReader();
-    reader.readAsDataURL(image);
-    reader.onloadend = () => {
-      uploadWithImage(reader.result);
-    };
-
-    const uploadWithImage = async (img: string) => {
-      try {
-        const post = await fetcher('http://localhost:4000/posts/', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${
-              document.cookie
-                ?.split('; ')
-                ?.find((value) => value?.includes('token'))
-                ?.split('=')[1]
-            }`,
-          },
-          body: JSON.stringify({
-            text,
-            image: img,
-          }),
-        });
-
-        setPosts((prevPosts) => [...prevPosts!, post]);
-      } catch (err) {
-        setError('Something went wrong');
-      } finally {
-        setIsLoading(false);
-      }
-    };
+      setPosts((prevPosts) => [...prevPosts!, post]);
+    } catch (err) {
+      setError('Something went wrong');
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   return {
-    createpost,
+    createPost,
     isLoading,
     error,
     message,
