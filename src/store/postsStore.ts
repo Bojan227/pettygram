c729eced6bd21e2c8252d01ee771474d1fda423f
Bojan Blazevski <blazevski.bojan1@gmail.com>
2023-01-28 17:29:40 +0100

    add number of posts field and method for setting the number of posts

--- src/store/postsStore.ts
+++ src/store/postsStore.ts
@@ -3,30 +3,42 @@ import { Post } from '../components/feed/types/feedTypes';
 
 type PostsStore = {
   posts: Post[];
+  numberOfPosts: number;
   addPost: (post: Post) => void;
   load: (posts: Post[]) => void;
   changeLikeState: (post: Post) => void;
   pagination: (posts: Post[]) => void;
+  setNumberOfPosts: (numberOfPosts: number) => void;
 };
 
 export const usePostsStore = create<PostsStore>((set) => ({
   posts: [],
+  numberOfPosts: 0,
   addPost: (post) =>
     set((state) => ({
+      ...state,
       posts: [...state.posts, post],
     })),
   load: (posts) =>
-    set(() => ({
+    set((state) => ({
+      ...state,
       posts: posts,
     })),
   changeLikeState: (post) =>
     set((state) => ({
+      ...state,
       posts: state.posts.map((prevPost) =>
         prevPost._id === post._id ? post : prevPost
       ),
     })),
   pagination: (posts) =>
     set((state) => ({
+      ...state,
       posts: [...state.posts, ...posts],
+    })),
+  setNumberOfPosts: (numberOfPosts) =>
+    set((state) => ({
+      ...state,
+      numberOfPosts,
     })),
 }));
