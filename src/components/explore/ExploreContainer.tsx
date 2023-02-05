6a9ccc66c9712b484abb7f7e40a3d9feb223a482
Bojan Blazevski <blazevski.bojan1@gmail.com>
2023-02-05 12:52:35 +0100

    add scroll to top on page render

--- src/components/explore/ExploreContainer.tsx
+++ src/components/explore/ExploreContainer.tsx
@@ -1,14 +1,18 @@
-import { Post } from '../feed/types/feedTypes';
+import { useEffect, useRef } from 'react';
 import './explore.css';
 import { v4 as uuidv4 } from 'uuid';
-import { Link } from 'react-router-dom';
+
 import ExploreCard from './ExploreCard';
 import { usePostsStore } from '../../store/postsStore';
 
 export default function ExploreContainer() {
   const { posts } = usePostsStore();
+  const refToTop = useRef<HTMLInputElement>(null);
+  useEffect(() => {
+    refToTop.current && refToTop.current.scrollIntoView({ behavior: 'smooth' });
+  });
   return (
-    <div className="explore-container">
+    <div ref={refToTop} className="explore-container">
       {posts.map((post) => (
         <ExploreCard key={uuidv4()} {...{ ...post }} />
       ))}
--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -2,10 +2,15 @@ import { ProfileCard } from './ProfileCard';
 import './profile.css';
 import { ProfileNav } from './ProfileNav';
 import { Outlet } from 'react-router-dom';
+import { useRef, useEffect } from 'react';
 
 export const Profile = () => {
+  const refToTop = useRef<HTMLInputElement>(null);
+  useEffect(() => {
+    refToTop.current && refToTop.current.scrollIntoView({ behavior: 'smooth' });
+  });
   return (
-    <div className="profile-container">
+    <div ref={refToTop} className="profile-container">
       <ProfileCard />
       <ProfileNav />
       <Outlet />
