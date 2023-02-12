426b02695be1c30cda0a1628d044964557a77f99
Bojan Blazevski <blazevski.bojan1@gmail.com>
2023-02-12 14:40:17 +0100

    add optional argument

--- src/hooks/useGetData.ts
+++ src/hooks/useGetData.ts
@@ -4,13 +4,14 @@ import fetcher from '../api/fetcher';
 interface UseGetDataProps {
   url: string;
   setState: React.Dispatch<React.SetStateAction<any | null>>;
+  options?: RequestInit;
 }
 
 export const useGetData = () => {
   const [isLoading, setIsLoading] = useState(false);
-  const getData = async ({ url, setState }: UseGetDataProps) => {
+  const getData = async ({ url, setState, options }: UseGetDataProps) => {
     setIsLoading(true);
-    const json = await fetcher(url);
+    const json = await fetcher(url, options);
     if (json) {
       setIsLoading(false);
     }
