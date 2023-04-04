32771c61317f39275398798d2e2dfcf991866894
Bojan227 <blazevski.bojan1@gmail.com>
2023-04-04 12:46:33 +0200

    add suggested user skeleton component

---
+++ src/components/feed/SuggestedUserSkeleton.tsx
@@ -0,0 +1,11 @@
+export default function SuggestedUserSkeleton() {
+  return (
+    <div className="user-skeleton-container">
+      <div>
+        <div className="skeleton-avatar"></div>
+        <div className="skeleton-author"></div>
+      </div>
+      <button className="skeleton-button"></button>
+    </div>
+  );
+}
